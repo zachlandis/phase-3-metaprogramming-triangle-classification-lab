737dd818f158e657428118973c0ea05174d6c1dc
Zach <zachlandis91@gmail.com>
2023-06-12 10:01:09 -0600

    commit tree b4487f2f7b425cb5a78061613f2a4b51d571820c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    validate_triangle
+      if side1 == side2 && side2 == side3
+        :equilateral
+      elsif side1 == side2 || side2 == side3 || side1 == side3
+        :isosceles
+      else
+        :scalene
+    end
+  end
+
+  def positive_sides?
+    [side1, side2, side3].all?(&:positive?)
+  end
+  
+  def triangle_inequality?
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  end
+
+  def validate_triangle
+    raise TriangleError unless positive_sides? && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
